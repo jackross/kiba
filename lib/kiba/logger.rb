f437b5f76b8556537c3691b8337600d4519abd39
Jack A Ross <jackross@jackmacpro2013.internal.technekes.com>
2016-01-11 12:05:11 -0500

    Rubocop-related changes

--- lib/kiba/logger.rb
+++ lib/kiba/logger.rb
@@ -4,8 +4,8 @@ module Kiba
 
     def at
       I18n.l Time.now.utc, format: :exact_at
-      rescue
-        Time.now.utc.strftime 'at %Y-%m-%d %H:%M:%S:%L'
+    rescue
+      Time.now.utc.strftime 'at %Y-%m-%d %H:%M:%S:%L'
     end
 
     def log(msg)
